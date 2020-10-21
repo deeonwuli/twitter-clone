c35db17d99d7b58836b201ff9e51209abd0ce118
deeonwuli <onwulidumebi@gmail.com>
2020-10-21 16:25:33 +0100

    sidebar component completed

--- twitter-clone/src/Sidebar.js
+++ twitter-clone/src/Sidebar.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import './Sidebar.css';
-import SidebarOption from './SidebarOption'
+import SidebarOption from './SidebarOption';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
-import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutlined';import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { Button } from '@material-ui/core'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             {/* Twitter icon */}
             <TwitterIcon />
-            <SidebarOption Icon={HomeIcon} text='Home' />
+
+            {/* SidebarOption */}
+            <SidebarOption active Icon={HomeIcon} text='Home' />
             <SidebarOption Icon={SearchIcon} text='Explore' />
             <SidebarOption Icon={NotificationsNoneIcon} text='Notifications' />
-            <SidebarOption />
-
-            {/* SidebarOption */}
-            <HomeIcon />
-
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
+            <SidebarOption Icon={EmailOutlinedIcon} text='Message' />
+            <SidebarOption Icon={BookmarkBorderOutlinedIcon} text='Bookmark' />
+            <SidebarOption Icon={ListAltIcon} text='Lists' />
+            <SidebarOption Icon={PersonOutlineOutlinedIcon} text='Profile' />
+            <SidebarOption Icon={MoreHorizIcon} text='More' />
 
             {/* Button -> Tweet */}
-
+            <Button variant='outlined' className='sidebar__tweet' fullWidth>Tweet</Button>
         </div>
     );
 }
--- twitter-clone/src/SidebarOption.js
+++ twitter-clone/src/SidebarOption.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './SidebarOption.css';
 
-function SidebarOption({ text, Icon }) {
+function SidebarOption({ active, text, Icon }) {
     return (
-        <div className='sidebarOption'>
-            {/* PROBLEM!!! 46:55  <Icon /> */}
+        <div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
+            <Icon />
             <h2>{text}</h2>
         </div>
     );
